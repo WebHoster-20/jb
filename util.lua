a9d6e64a7057b0d9eecd6c0f38d7887c66bb53f9
WebHoster-20 <WebHost166@proton.me>
2025-09-06 08:22:23 -0700

    Create util.lua

---
+++ util.lua
@@ -0,0 +1,37 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local hrp = character:WaitForChild("HumanoidRootPart")
+local humanoid = character:WaitForChild("Humanoid")
+workspace.Gravity = 0
+humanoid:ChangeState(Enum.HumanoidStateType.Physics)
+
+local function moveTo(target, speed)
+    local dt = RunService.Heartbeat:Wait()
+    local direction = (target - hrp.Position)
+    local distance = direction.Magnitude
+    if distance < 1 then return true end
+    local moveStep = direction.Unit * math.min(speed * dt, distance)
+    hrp.CFrame = hrp.CFrame + moveStep
+    return false
+end
+
+spawn(function()
+    while hrp.Position.Y < 350 - 1 do
+        moveTo(Vector3.new(hrp.Position.X, 350, hrp.Position.Z), 500)
+    end
+    while (Vector3.new(hrp.Position.X, 0, hrp.Position.Z) - Vector3.new(targetPos.X, 0, targetPos.Z)).Magnitude > 1 do
+        moveTo(Vector3.new(targetPos.X, 350, targetPos.Z), 120)
+    end
+    while math.abs(hrp.Position.Y - targetPos.Y) > 1 do
+        local distance = math.abs(hrp.Position.Y - targetPos.Y)
+        local speed = 300
+        if distance <= 20 then
+            speed = 30
+        end
+        moveTo(Vector3.new(targetPos.X, targetPos.Y, targetPos.Z), speed)
+    end
+    workspace.Gravity = 196.2
+    humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
+end)
